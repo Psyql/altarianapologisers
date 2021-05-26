ef3be3466aa1b0118321f5570ea27ffc64a5f914
Psyql <tplantchaney@gmail.com>
2021-05-26 11:37:34 +1000

    initial commit
    site content

---
+++ altarian/js/demo.js
@@ -0,0 +1,21 @@
+var events = [
+  {'Date': new Date(2021, 4, 1), 'Title': 'Weekly Community Match: 2pm'},
+  {'Date': new Date(2021, 4, 8), 'Title': 'Weekly Community Match: 2pm'},
+  {'Date': new Date(2021, 4, 15), 'Title': 'Weekly Community Match: 2pm'},
+  {'Date': new Date(2021, 4, 19), 'Title': 'Match vs Belcerabons: 10am at the ABUCG'},
+  {'Date': new Date(2021, 4, 22), 'Title': 'Weekly Community Match: 2pm'},
+  {'Date': new Date(2021, 4, 29), 'Title': 'Weekly Community Match: 2pm'},
+  {'Date': new Date(2021, 5, 5), 'Title': 'Weekly Community Match: 2pm'},
+  {'Date': new Date(2021, 5, 12), 'Title': 'Weekly Community Match: 2pm'},
+  {'Date': new Date(2021, 5, 14), 'Title': 'Match vs Grebulons: 9am at the ABUCG'},
+  {'Date': new Date(2021, 5, 19), 'Title': 'Weekly Community Match: 2pm'},
+  {'Date': new Date(2021, 5, 26), 'Title': 'Weekly Community Match: 2pm'},
+  {'Date': new Date(2021, 6, 3), 'Title': 'Weekly Community Match: 2pm'},
+  {'Date': new Date(2021, 6, 10), 'Title': 'Weekly Community Match: 2pm'},
+  {'Date': new Date(2021, 6, 17), 'Title': 'Weekly Community Match: 2pm'},
+  {'Date': new Date(2021, 6, 24), 'Title': 'Weekly Community Match: 2pm'},
+  {'Date': new Date(2021, 6, 31), 'Title': 'Weekly Community Match: 2pm'},
+];
+var settings = {};
+var element = document.getElementById("caleandar");
+caleandar(element, events, settings);
